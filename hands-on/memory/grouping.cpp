d351c7d26aae89e6d0992a57365c882f9e381a3b
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2018-10-17 11:13:06 +0200

    more doc

--- hands-on/memory/grouping.cpp
+++ hands-on/memory/grouping.cpp
@@ -68,6 +68,12 @@ public:
   auto nElements() const {
     return totElements;
   }
+
+  // fake weight
+  float weight(uint32_t el) const {
+    // assert(el< totElements);
+    return 0.01f*float(el);
+  }
   
  private:
   uint32_t ng;
@@ -80,6 +86,7 @@ public:
 
 Generator generator;
 
+#include<map>  // not necessarely the best choice!
 #include<unordered_map>  // not necessarely the best choice!
 void one(bool doprint) {
  if (doprint) stop("before generation");
@@ -91,17 +98,31 @@ void one(bool doprint) {
   if (doprint) stop("aftert generation");
 
   // here you have to find the first set of groups
-  std::unordered_map<int,int> count;  // in std the default constructor of int IS int(0)
-  for (auto i=0U;i<ntot;++i) ++count[generator.protoGroup(i)];
-  if (doprint) std::cout << "--- Found " << count.size() << " groups" << std::endl;
+  // in principle you are allowed to call generator.protoGroup only ONCE per element as it is faking an clustering algo
 
+  // this is just a test to verify the generator
+  //  std::unordered_map<int,int> count;  // in std the default constructor of int IS int(0)
+  std::map<int,int> count;  // in std the default constructor of int IS int(0)
+  for (auto i=0U;i<ntot;++i) ++count[generator.protoGroup(i)];
+  if (doprint) std::cout << "--- Found " << count.size() << " proto-groups" << std::endl;
+
+  if (doprint) stop("aftert protoGroups");
 
   
   // and then split them in the final set
 
+  // again a test of consistency....
+  for (auto i=0U;i<ntot;++i) {
+    auto sub = generator.split(generator.protoGroup(i),i); // no, you are not allowed to use generator.protoGroup here!!
+    assert(sub<2);
+  }
+  if (doprint) stop("after splitting");
+
 
   // at the end you should be able to tell with element is in which final group...
+  // for instance demonstrate you can compute the total "weight" of a group calling generator.weight(j);
 
+  
  if (doprint) stop("end of algo");
  
 }
--- memory/MemoryAbuse.markdown
+++ memory/MemoryAbuse.markdown
@@ -1,5 +1,5 @@
 ---
-title: Avoi dmemory abuse
+title: Avoid memory abuse
 layout: main
 category: memory
 ---
@@ -15,9 +15,14 @@ In addition the use of "sparse" data structures may imply
 Open [grouping.cpp]({{site.exercises_repo}}/hands-on/memory/grouping.cpp)
 
 The exercise consists in building the relationship between elements and groups
-(as in cluster algorithm for instance) so that one can find in which group an element belongs and perform operations as "compute the sum/average.. over some features of the elelements in each group (the total charge/mass, the baricenter etc)
+(as in cluster algorithm for instance) so that one can find in which group an element belongs to
+and can perform operations such as "compute the sum/average.." over some features of the elelements in each group
+(find the total charge/mass, the baricenter etc)
 
 Only the total number of elements is given (elements numbered from 0 to N-1).
 The assumption is that the groups shall be discovered in the exercise even if the "grouping" algorithm is left undefined and is FAKED by some simple assignement.
 
 There is an assumption that the algorithm is in two steps: first it find protoGroups and then it spits (some of them) in two
+
+
+Not in the exercise: because is coded for test purpose, just compare the performance of "map" vs "unordered_map"
