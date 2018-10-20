08994cd359c444a70b516d7642ef95a0913bf3be
Francesco Giacomini <giaco at cnaf dot infn dot it>
2018-10-20 17:20:52 +0200

    c++: review move hands-on

--- cpp/move.md
+++ cpp/move.md
@@ -8,14 +8,17 @@ The goal of this exercise is to appreciate the performance effect of move
 semantics and we'll do in the context of returning a value from a function.
 
 Open the program [`move.cpp`]({{site.exercises_repo}}/hands-on/cpp/move.cpp). It
-contains the `make_vector` function introduced in one of the previous exercises.
+contains a slight variation of the `make_vector` function introduced in one of
+the previous exercises.
 
 Measure the time it takes to execute it applying the following variations:
 
 * the result is returned from the function
 
+* the result is passed to the function as an output parameter (by reference or
+  by pointer)
+
 * the result is return from the function, but the Return Value Optimization is
   disabled (how?)
 
-* the result is passed to the function as an output parameter (by reference or
-  by pointer)
+Is there any differenc
---
+++ hands-on/cpp/move_solution.cpp
@@ -0,0 +1,81 @@
+#include <random>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <chrono>
+#include <functional>
+
+std::vector<int> make_vector_rvo(int N, std::function<int()> gen)
+{
+  std::vector<int> result;
+
+  result.reserve(N);
+  std::generate_n(std::back_inserter(result), N, gen);
+
+  return result;
+}
+
+void make_vector_out(std::vector<int> &result, int N, std::function<int()> gen)
+{
+  result.reserve(N);
+  std::generate_n(std::back_inserter(result), N, gen);
+}
+
+std::vector<int> make_vector_norvo(int N, std::function<int()> gen)
+{
+  // "disable" RVO by mixing named and unnamed return
+  if (N > 0) {
+    std::vector<int> result;
+
+    result.reserve(N);
+    std::generate_n(std::back_inserter(result), N, gen);
+
+    return result;
+  } else {
+    return std::vector<int>{};
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  int const N = (argc > 1) ? std::atoi(argv[1]) : 1000000;
+
+  std::random_device rd;
+  std::mt19937 eng{rd()};
+
+  int const MAX_N = 100;
+  std::uniform_int_distribution<int> dist{1, MAX_N};
+
+  std::function<int()> gen = [&]{ return dist(eng); };
+
+  { // warmup
+    std::vector<int> v = make_vector_rvo(N, gen);
+  }
+  {
+    auto t0 = std::chrono::high_resolution_clock::now();
+    std::vector<int> v = make_vector_rvo(N, gen);
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::cout << "rvo: " << N << " random ints in "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count()
+              << " ms\n";
+  }
+  {
+    auto t0 = std::chrono::high_resolution_clock::now();
+    std::vector<int> v = make_vector_norvo(N, gen);
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::cout << "norvo: " << N << " random ints in "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count()
+              << " ms\n";
+  }
+  {
+    auto t0 = std::chrono::high_resolution_clock::now();
+    std::vector<int> v;
+    make_vector_out(v, N, gen);
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::cout << "out: " << N << " random ints in "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count()
+              << " ms\n";
+  }
+};
